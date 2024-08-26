dc0d7c0de52989f4570f1a5a9330ec7c2286eb7e
petrsimunek <petr.simunek@outlook.com>
2024-08-26 17:26:08 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# nfc-reader
+# NFC Reader
+
 NFC card reader with display, HID functionality and 3D printed case
+
+## Motivation & backstory
+
+User who is maintaining EV chargers asked for a simple tool, where he would be able to see card or chip number instantly. Chips comes sometimes with the number engraved on it, but some suppliers delivers cards without numbers.
+
+So our user had to go to the EV charger, beep the card, go back to computer, and check the EV management for the "unauthorized card".
+
+He asked for a solution, which would show him the number, and/or put the number to the computer.
+
+I tried USB/NFC readers, or some flipper-like devices, but those were too complicated.
+
+## How it works
+
+Solution is built on arduino with NFC reader and simple display. User scan the card, and the number is displayed. The arduino has the HID feature enabled, so it directly inputs the card number to the connected computer, followed by TAB key.
+
+SIMPLE!
+
+## What is needed
+
+- Arduino (pick model which supports HID feature - Arduino Pro Micro ATmega32U4 5V 16MHz) [link](https://dratek.cz/arduino/960-leonardo-pro-micro.html)
+- PN532 NFC card reader [link](https://dratek.cz/arduino/2005-rfid-ic-ctecka-karet-13.56mhz-modul-pro-arduino-pn532-nfc.html)
+- 0,96'' OLED display [link](https://dratek.cz/arduino/891-oled-display-spi-bily.html)
+
+Plus
+
+- Common tools + tool for soldering
+- 3D printer
+- Glue
+
---
+++ arduino/project.ino
@@ -0,0 +1,121 @@
+// include libs
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+#include <U8glib.h>
+#include <Keyboard.h>
+
+// init I2C communication with NFC reader
+PN532_I2C pn532i2c(Wire);
+// init instance of NFC reader
+PN532 nfc(pn532i2c);
+
+// init OLED display
+U8GLIB_SSD1306_128X64 myOled(U8G_I2C_OPT_NONE);
+long int rewrite = 0;               // timer for rewrite the display
+int scannedNFCCounter = 0;          // counter of scanned NFC tags since start
+
+// init var for decimal value of the NFS tag
+unsigned long decimalValue = 0;       // ID in dec
+String stringDecimalValue;            // ID in string
+
+void setup() {
+  // keyboard HID start
+  Keyboard.begin();
+  // start serial communication
+  Serial.begin(9600);
+  // greetings on serial after a small delay
+  delay(3000);
+  Serial.println("Hello!");
+  // start NFC communication
+  nfc.begin();
+  // set max no of NFC read attempts
+  // set to approx. one second
+  nfc.setPassiveActivationRetries(0xFF);
+  // configure NFC for reading tags
+  nfc.SAMConfig();
+  // debug info for user
+  Serial.println("Ready");
+}
+
+void loop() {
+  // init vars, where the results will be stored
+  boolean success;                          // successfull reading
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // unique ID of the NFC tag
+  uint8_t uidLength;                        // length of ID
+
+  //display refresh
+  if (millis()-rewrite > 100) {
+    // following set of commands refresh display
+    // refresh display
+    myOled.firstPage();
+    do {
+      displayWrite();
+    } while( myOled.nextPage() );
+    // save last time of rewrite
+    rewrite = millis();
+  }
+
+  // start reading nearby tags, results will be stored to variables
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+  // if the read is success, print results to serial
+  if (success) {
+    Serial.println("NFC tag found!");
+    Serial.print("UID length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+    Serial.print("UID value: ");
+    for (uint8_t i = 0; i < uidLength; i++) {
+      Serial.print(" 0x"); Serial.print(uid[i], HEX);
+      decimalValue = (decimalValue << 8) | uid[i];
+    }
+    stringDecimalValue = String(decimalValue);
+    Serial.println();
+    Serial.println("UID value complete: ");
+    Serial.println(stringDecimalValue);
+    // send to keyboard
+    for (int i = 0; i < stringDecimalValue.length(); i++) {
+      // get current number
+      int digit = stringDecimalValue[i] - '0';
+      // send it to keyboard
+      typeNumber(digit);
+    }
+    Keyboard.write(KEY_TAB);
+    //increase number of scanned NFCs
+    scannedNFCCounter++;
+    // wait 1 second before next read
+    delay(1000);
+  }
+  
+}
+
+void displayWrite() {
+  // set font (approx 15*4 chars available)
+  myOled.setFont(u8g_font_unifont);
+  // set init position
+  myOled.setPrintPos(0, 10);
+  // write text to the display
+  myOled.print("Last NFC code:");
+  myOled.setPrintPos(0, 25);
+  myOled.print(stringDecimalValue);
+  myOled.setPrintPos(0, 40);
+  myOled.print("Scanned:");
+  myOled.setPrintPos(40, 55);
+  myOled.print(scannedNFCCounter);
+  myOled.print(" NFCs");
+}
+
+void typeNumber(int digit) {
+  // simulate numpad
+  // added this due the keyboard lang settings
+  switch (digit) {
+    case 0: Keyboard.write(KEY_KP_0); break;
+    case 1: Keyboard.write(KEY_KP_1); break;
+    case 2: Keyboard.write(KEY_KP_2); break;
+    case 3: Keyboard.write(KEY_KP_3); break;
+    case 4: Keyboard.write(KEY_KP_4); break;
+    case 5: Keyboard.write(KEY_KP_5); break;
+    case 6: Keyboard.write(KEY_KP_6); break;
+    case 7: Keyboard.write(KEY_KP_7); break;
+    case 8: Keyboard.write(KEY_KP_8); break;
+    case 9: Keyboard.write(KEY_KP_9); break;
+  }
+}
